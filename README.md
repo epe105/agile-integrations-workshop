915fdf5f40b87508f6f6f2650dacf37063926c6b
Manny Evangelista <19913916+epe105@users.noreply.github.com>
2018-12-12 14:40:46 -0500

    initial working provisioning

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # agile-integrations-workshop
+
+##Prerequisites
+Must have an OpenShift Environment
+
+## Run ansible playbook
+
+$ ansible-playbook -i install/ansible/inventory/workshop.inventory install/ansible/playbooks/openshift/install.yml
+
+## Delete
+
+$ delete.sh
---
+++ install/delete.sh
@@ -0,0 +1,12 @@
+oc login -u shadowman -p r3dh4t1! https://ocp-ai.redhatgov.io --insecure-skip-tls-verify
+
+oc delete project user1
+oc delete project tools1
+oc delete project user2
+oc delete project tools2
+oc delete project gogs
+oc delete project microcks
+oc delete project 3scale
+oc delete project international
+oc delete project apicurio
+oc delete project sso
